304c58449877e856bbd6d1a119718802cc559795
NokibReja <nakib7reja@gmail.com>
2025-12-19 23:49:04 +0600

    filter news

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
-    const {id} = useParams();
-    console.log(id)
+    const { id } = useParams();
+    const data = useLoaderData();
+    // console.log(data)
+    const [categoryNews, setCategoryNews] = useState([])
+
+    useEffect(() => {
+        const filteredNews = data.filter(news => news.category_id == id);
+        console.log(filteredNews);
+
+        setCategoryNews(filteredNews);
+
+    }, [data, id])
+
     return (
         <div>
-            Category news -{id}
+            Category news -{categoryNews.length}
         </div>
     );
 };
