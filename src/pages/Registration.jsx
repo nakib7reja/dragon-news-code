8b2f811551f161f6a0b515e4fde4d7aa8254ec33
NokibReja <nakib7reja@gmail.com>
2025-12-23 23:13:30 +0600

    google auth

--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,38 +1,94 @@
-import React from 'react';
+// import React, { use } from 'react';
+// import { Link } from 'react-router';
+// import { AuthContext } from '../provider/AuthProvider';
+
+// const Registration = () => {
+//     const { createUser } = use(AuthContext)
+
+import { useContext } from 'react';
 import { Link } from 'react-router';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Registration = () => {
+    const auth = useContext(AuthContext);
+
+    const { createUser } = auth || {};
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const name = form.name.value;
+        const photo = form.photo.value;
+        const email = form.email.value;
+        const password = form.pass.value;
+        console.log({ name, photo, email, password })
+
+        createUser(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+            })
+            .catch((error) => {
+                // const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(errorMessage)
+            });
+    }
     return (
         <div className='flex justify-center min-h-screen items-center'>
             <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-5">
                 <h2 className="font-semibold text-2xl text-center">Registration your account</h2>
-                <div className="card-body">
+                <form onSubmit={handleRegister} className="card-body">
                     <fieldset className="fieldset">
 
                         {/* Name */}
                         <label className="label">Name</label>
-                        <input type="name" className="input" placeholder="Name" />
+                        <input
+                            name='name'
+                            type="text"
+                            className="input"
+                            placeholder="Name"
+                            required
+                        />
 
                         {/* Photo Url */}
                         <label className="label">Photo URL</label>
-                        <input type="name" className="input" placeholder="Photo URL" />
+                        <input
+                            name='photo'
+                            type="text"
+                            className="input"
+                            placeholder="Photo URL"
+                        />
 
                         {/* email */}
                         <label className="label">Email</label>
-                        <input type="email" className="input" placeholder="Email" />
+                        <input
+                            name='email'
+                            type="email"
+                            className="input"
+                            placeholder="Email"
+                            required
+                        />
 
                         {/* password */}
                         <label className="label">Password</label>
-                        <input type="password" className="input" placeholder="Password" />
+                        <input
+                            name='pass'
+                            type="password"
+                            className="input"
+                            placeholder="Password"
+                            required
+                        />
 
                         {/* <div><a className="link link-hover">Forgot password?</a></div> */}
 
-                        <button className="btn btn-neutral mt-4">Registration</button>
+                        <button type='submit' className="btn btn-neutral mt-4">Registration</button>
+
                         <p className='font-semibold text-center mt-5'>Already Have an account?{'  '}
                             <Link className='text-secondary' to={'/auth/login'}>Login</Link>
                         </p>
                     </fieldset>
-                </div>
+                </form>
             </div>
         </div>
     );
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,9 +1,56 @@
-import React, { createContext } from 'react';
+import { createContext, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import app from '../firebase/firebase.config';
 
-export const AuthContext = createContext()
+export const AuthContext = createContext(null);
 
-const AuthProvider = () => {
-    return <AuthContext></AuthContext>
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const value = {
+    user,
+    setUser,
+    createUser,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
+
+
+
+
+// import React, { createContext, useState } from 'react';
+// import app from '../firebase/firebase.config';
+// import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+// export const AuthContext = createContext()
+
+// const auth = getAuth(app)
+
+// const AuthProvider = ({children}) => {  
+//     const [user, setUser] =useState(null)
+//     const createUser = (email, password) => {
+//         return createUserWithEmailAndPassword(auth, email, password)
+//     };
+
+//     const authData ={
+//         user,
+//         setUser,
+//         createUser
+//     }
+    
+//     return <AuthContext value={authData}>{children}</AuthContext>
+// };
+
+// export default AuthProvider;
